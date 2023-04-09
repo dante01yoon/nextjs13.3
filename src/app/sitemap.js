fd940bd27442927e960e08a7a9a749a30c2298d0
dante01yoon <bunggl@naver.com>
2023-04-09 20:03:25 +0900

    feat: file-basd-config done

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,12 +1,4 @@
 export default async function sitemap() {
-  // const res = await fetch("/api/hello")
-  // const allPosts = await res.json();
-  
-  // const posts = allPosts.map((post) => ({
-  //   url: `https://acme.com/blog/${post.slug}`,
-  //   lastModified: new Date().toTimeString()
-  // }))
-
   const posts = [
     {
       url: `https://acme.com/blog/1`,
@@ -17,6 +9,5 @@ export default async function sitemap() {
       lastModified: new Date().toTimeString()
     }
   ]
-  // console.log("posts: ", posts)
   return posts
 }
