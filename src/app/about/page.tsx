6fc06110e69ffb4ba58b5dfda0705978f25081ab
dante01yoon <bunggl@naver.com>
2023-04-12 00:13:59 +0900

    fix: link href

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,7 +14,7 @@ export default function About() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/about/page.js</code>
         </p>
-        <Link href="/">go to home page</Link>
+        <Link href="/nextjs13.3/">go to home page</Link>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
